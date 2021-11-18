6c97424a18f349856e9658a2d53e4d1dedf57b57
CelesteOjeda <malu221098@gmail.com>
2021-11-18 13:30:42 -0300

    Busqueda en json segun provincia ingresada

--- SMN.py
+++ SMN.py
@@ -1,12 +1,18 @@
 import requests
 import json
 url='https://ws.smn.gob.ar/map_items/weather'
-#args= {'nombre': 'Eduardo','curso':'python'}
 
-response = requests.get(url)#, #params=args)
+response = requests.get(url)
 if response.status_code ==200: #si realiza la conexion
 	#response_json=response.json() #es un diccionario??
 	#print(response_json)
 	response_json=json.loads(response.text)
-	print(response_json)
+	a=input('Ingrese su peticion: ')
+	for i in response_json:
+		if (i)['province']==a:
+			idj=i
+			print(idj)
+		else:
+			continue
+	#print((response_json[0])['name']) #son 217 registros
 
