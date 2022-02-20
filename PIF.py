4a8a317719701304f8d512931f7e79beeb96113b
CelesteOjeda <malu221098@gmail.com>
2022-02-20 11:04:16 -0300

    Main

--- PIF.py
+++ PIF.py
@@ -1,26 +1,26 @@
 #Probabilidad de incendio forestal
-from SMN_API import *
-from CalculoProbIncendio import *
+import SMN_API as API
+import CalculoProbIncendio
 
 #-----------Conexion con la API y almacenado de diccionario
 url='https://ws.smn.gob.ar/map_items/weather' #direccion en la que se encuentra la API del Servicio Metereologico Nacional
-json=conexion(url) #Se realiza la conexion con la API y se guarda su diccionario en una variable "json" para su manejo
+json=API.conexion(url) #Se realiza la conexion con la API y se guarda su diccionario en una variable "json" para su manejo
 
 #-----------Seleccion de Provincia
 print("\nProvincias:\n")
-MostrarLista(ListaProv(json)) #muestra la lista de provincias
+API.MostrarLista(API.ListaProv(json)) #muestra la lista de provincias
 
 p=input('\nElija una Provincia: ') 
-prov=ListaProv(json)[int(p)] #busca la provincia en la lista correspondiente al indice ingresado p
+prov=API.ListaProv(json)[int(p)] #busca la provincia en la lista correspondiente al indice ingresado p
 
 #-----------Seleccion de Localidad
 print("\nLocalidades de ",prov,"\n")
-MostrarLista(ListaLoc(prov,json)) #muestra la lista de localidades
+API.MostrarLista(API.ListaLoc(prov,json)) #muestra la lista de localidades
 
 l=input('\nElija una localidad: ')
-loc=ListaLoc(prov,json)[int(l)]
+loc=API.ListaLoc(prov,json)[int(l)]
 
 #-----------Resultado de Probabilidad de Incendio Forestal
-h,velv,t=Clima(prov,loc,json) #almacena la humedad,temperatura y velocidad del viento de la localidad solicitada
-probabilidad(h,velv,t) #clasifica la probabilidad de incendio forestal
+h,velv,t=API.Clima(prov,loc,json) #almacena la humedad,temperatura y velocidad del viento de la localidad solicitada
+CalculoProbIncendio.probabilidad(h,velv,t) #clasifica la probabilidad de incendio forestal
 
